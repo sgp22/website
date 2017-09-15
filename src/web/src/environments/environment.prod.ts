5a0c4ecd3d62c2667fc08a41dfb8b240cb980e8c
John Macaluso <John.Macaluso@infor.com>
2017-09-15 11:50:02 -0400

    updated environmen prod

--- src/web/src/environments/environment.prod.ts
+++ src/web/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: "http://docs-site-staging.us-east-1.elasticbeanstalk.com/api/v2/",
+  dummyApi: "https://jsonplaceholder.typicode.com/posts"
 };
