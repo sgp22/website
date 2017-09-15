f7093153fd6bc61bd65ac6a8b61bf96f12f94fe0
Artem Golub <artemgolub@gmail.com>
2017-09-15 10:33:01 -0400

    More backend base setup.

--- src/app/home/models.py
+++ src/app/home/models.py
@@ -3,7 +3,19 @@ from __future__ import absolute_import, unicode_literals
 from django.db import models
 
 from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.api import APIField
 
 
 class HomePage(Page):
-    pass
+    content = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('content', classname="full"),
+    ]
+    # Export fields over the API
+    api_fields = [
+        APIField('title'),
+        APIField('content')
+    ]
