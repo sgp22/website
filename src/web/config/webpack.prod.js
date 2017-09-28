d08bd0337e13a67fd9d98bc4c14a329b46675eaa
John Macaluso <John.Macaluso@infor.com>
2017-09-28 14:47:27 -0400

    removed AoTplugin from webpack prod

--- src/web/config/webpack.prod.js
+++ src/web/config/webpack.prod.js
@@ -2,7 +2,6 @@ const path = require("path");
 const webpackMerge = require("webpack-merge");
 const commonConfig = require("./webpack.common.js");
 const DefinePlugin = require("webpack/lib/DefinePlugin");
-const { AotPlugin } = require("@ngtools/webpack");
 const nodeModules = path.join(process.cwd(), "node_modules");
 const { CommonsChunkPlugin } = require("webpack").optimize;
 
