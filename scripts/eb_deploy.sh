5a2084d4ac1ed29bc3f251bec29cd05306feba4f
Artem Golub <artemgolub@gmail.com>
2017-10-24 02:15:57 -0400

    .gitignore small change.

--- scripts/eb_deploy.sh
+++ scripts/eb_deploy.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 
-COMMIT_MSG=$(git log -1 --pretty=%B)
-git add --all
-git commit -m "$COMMIT_MSG"
-eb deploy
-git reset HEAD~
+ROOTDIR="$( cd "$(dirname `dirname "${BASH_SOURCE[0]}"` )" && pwd )"
+
+sed -e 'static/ s/^#*/#/' -i '' "$ROOTDIR/.gitignore"
+
+#COMMIT_MSG=$(git log -1 --pretty=%B)
+#git add --all
+#git commit -m "$COMMIT_MSG"
+#eb deploy
+#git reset HEAD~
+
+#git checkout .gitignore
