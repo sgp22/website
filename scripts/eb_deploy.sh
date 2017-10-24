277c5fe2e215d37dd1920160ec6665e29f20334a
Artem Golub <artemgolub@gmail.com>
2017-10-24 02:41:50 -0400

    eb_deploy script change.

--- scripts/eb_deploy.sh
+++ scripts/eb_deploy.sh
@@ -3,12 +3,15 @@
 
 ROOTDIR="$( cd "$(dirname `dirname "${BASH_SOURCE[0]}"` )" && pwd )"
 
-sed -e 'static/ s/^#*/#/' -i '' "$ROOTDIR/.gitignore"
+line_old='src/web/dist'
+line_new='#src/web/dist'
+sed -e "s%$line_old%$line_new%g" -i '' "$ROOTDIR/.gitignore"
 
-#COMMIT_MSG=$(git log -1 --pretty=%B)
-#git add --all
-#git commit -m "$COMMIT_MSG"
-#eb deploy
-#git reset HEAD~
+COMMIT_MSG=$(git log -1 --pretty=%B)
+git add --all
+git commit -m "$COMMIT_MSG"
+eb deploy
+git reset HEAD~
 
-#git checkout .gitignore
+git status
+git checkout .gitignore
