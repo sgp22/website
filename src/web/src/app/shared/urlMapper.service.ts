311f0239905a1a38bf4027d036595140e6908de7
Dave Hall <daveghall@gmail.com>
2017-10-25 17:20:23 -0400

    Add unit test for urlMapper

--- src/web/src/app/shared/urlMapper.service.ts
+++ src/web/src/app/shared/urlMapper.service.ts
@@ -6,13 +6,16 @@ export class UrlMapper {
   map(tree: UrlTree): string {
     const segments = tree.root.children.primary.segments;
     const paths = [];
+
+    if (segments.length !== 4) {
+      throw new Error('Unexpected path segments: expected 4');
+    }
+
     for (let i = 0; i < segments.length; i++) {
       paths[i] = segments[i].path;
     }
 
-    let libName = '';
-    let version = '';
-    let compName = '';
+    let libName, version, compName = '';
 
     if (paths[1] === 'tempo') {
       libName = 'iux';
