35afb303de11feb9b930fd2851c84789110dd3e6
Dave Hall <daveghall@gmail.com>
2017-10-25 17:23:09 -0400

    Add unit test for urlFetcher

---
+++ src/web/src/app/shared/urlFetcher.service.spec.ts
@@ -0,0 +1,21 @@
+import { UrlFetcher } from './urlFetcher.service';
+import { HttpClient } from '@angular/common/http';
+import { HttpHandler } from '@angular/common/http';
+
+describe('url Fetcher', () => {
+  let urlFetcher: UrlFetcher;
+  let http: HttpClient;
+  let handler: HttpHandler;
+  let correctPath = '';
+
+  beforeEach(() => {
+    http = new HttpClient(handler);
+    urlFetcher = new UrlFetcher(http);
+    correctPath= 'http://docs-site-staging.us-east-1.elasticbeanstalk.com/api/docs/1.0.0/adaptive.json';
+  });
+
+  it('should create observable to subscribe', (() => {
+    expect(urlFetcher.getDocs(correctPath)).toBeTruthy();
+  }));
+
+});
