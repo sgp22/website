b93080f906d886609422e8c47ea2387b951193cf
Dave Hall <daveghall@gmail.com>
2017-11-16 11:34:45 -0500

    Add basic docs page tests

---
+++ src/web/e2e/docs.e2e-spec.ts
@@ -0,0 +1,20 @@
+import { DocsPage} from './docs.po';
+
+describe('ux-site Docs', () => {
+  let page: DocsPage;
+
+  beforeEach(() => {
+    page = new DocsPage();
+  });
+
+  it('should display docs h1 title', () => {
+    page.navigateTo();
+    expect(page.getHeaderText()).toEqual('Tabs');
+  });
+
+  it('should display nav', () => {
+    page.navigateTo();
+    expect(page.getNavText()).toContain('Version');
+    expect(page.getNavText()).toContain('Tabs');
+  });
+});
