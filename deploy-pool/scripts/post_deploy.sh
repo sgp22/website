68f6ea43bb0186719fbacae2a9b1630f1188934e
Artem Golub <artemgolub@gmail.com>
2017-12-01 10:16:32 -0500

    Minor tweaks to deployment scripts.

--- deploy-pool/scripts/post_deploy.sh
+++ deploy-pool/scripts/post_deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Waiting for 20 seconds to the swarm service
+# a little bit of time to start the container.
+#
+# TODO: Check if the service is running and then
+#       update nginx config.
+sleep 20
 
+# Reload nginx config.
 curl -X POST -u admin:n98Y-uhPb-llGa-LdUl http://usalvlhlpool1/swarm/update_config
 curl -X POST -u admin:n98Y-uhPb-llGa-LdUl http://usalvlhlpool1/swarm/reload_nginx
