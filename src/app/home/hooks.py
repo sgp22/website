64d789edd29ef6de9b5da6ee2b34feb1076d1529
John Macaluso <John.Macaluso@infor.com>
2017-12-06 14:34:22 -0500

    stached

--- src/app/home/hooks.py
+++ src/app/home/hooks.py
@@ -1,5 +1,13 @@
 from wagtail.wagtailcore import hooks
+from django.utils.html import format_html, format_html_join
+from django.conf import settings
 
 @hooks.register('construct_main_menu')
 def hide_snippets_menu_item(request, menu_items):
   menu_items[:] = [item for item in menu_items if item.name != 'snippets']
+
+@hooks.register('insert_editor_js')
+def editor_js():
+  return format_html(
+      '<script src="js/editor.js"></script>',
+  )
