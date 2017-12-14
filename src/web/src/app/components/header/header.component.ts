4033f3138d3e255bd6c5d35b60b1f18ae26db48b
John Macaluso <John.Macaluso@infor.com>
2017-12-14 15:59:58 -0500

    missing semicolon

--- src/web/src/app/components/header/header.component.ts
+++ src/web/src/app/components/header/header.component.ts
@@ -39,7 +39,7 @@ export class HeaderComponent implements OnInit, AfterViewInit, DoCheck {
       if (event instanceof NavigationEnd) {
         this.navToggle = false;
       }
-    })
+    });
 
   }
 
