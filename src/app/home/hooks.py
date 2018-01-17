015d013148311ac4a966889e9528f077a648bc71
Christopher Lepore <chris.lepore@infor.com>
2018-01-17 12:29:32 -0500

    Fixes #271 by allowing tables

--- src/app/home/hooks.py
+++ src/app/home/hooks.py
@@ -1,10 +1,21 @@
 from wagtail.wagtailcore import hooks
+from wagtail.wagtailcore.whitelist import allow_without_attributes
 from django.utils.html import format_html, format_html_join
 from django.conf import settings
+
+@hooks.register('construct_whitelister_element_rules')
+def whitelister_element_rules():
+    return {
+        'table': allow_without_attributes,
+        'tr': allow_without_attributes,
+        'td': allow_without_attributes
+    }
+
 
 @hooks.register('construct_main_menu')
 def hide_snippets_menu_item(request, menu_items):
   menu_items[:] = [item for item in menu_items if item.name != 'snippets']
+
 
 @hooks.register('insert_editor_js')
 def editor_js():
