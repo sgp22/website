d0a90bc2108cdd6dd9a023457857d473c5551c15
John Macaluso <John.Macaluso@infor.com>
2018-01-31 14:22:33 -0500

    fixed hasGrandchildren logic

--- src/web/src/app/components/sidebar-nav/sidebar-nav.component.ts
+++ src/web/src/app/components/sidebar-nav/sidebar-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Input } from '@angular/core';
 import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
 import { PagesService } from '../../shared/pages.service';
 
