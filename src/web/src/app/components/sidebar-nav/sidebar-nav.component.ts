9ce8c416df856772e5f787440dc0284399f71a81
John Macaluso <John.Macaluso@infor.com>
2018-02-14 11:43:37 -0500

    basic structure on the guidelines side

--- src/web/src/app/components/sidebar-nav/sidebar-nav.component.ts
+++ src/web/src/app/components/sidebar-nav/sidebar-nav.component.ts
@@ -11,11 +11,10 @@ export class SidebarNavComponent implements OnInit {
 
   @Input() sidebarData;
   @Input() section;
-  // @Input() hasGrandchildren;
   public sidebarNav: any;
   public sectionTitle: any;
   public grandChildren: boolean;
-  public hasGrandchildren: boolean;
+  public loading: boolean;
 
   constructor(
     private pagesService: PagesService,
@@ -23,54 +22,50 @@ export class SidebarNavComponent implements OnInit {
     private route: ActivatedRoute,
   ) {}
 
-  ngOnInit() {
-
-    // this.sidebarNav = this.sidebarData;
-    // this.sectionTitle = this.section;
-    // this.grandChildren = this.hasGrandchildren;
-
-    // this.router.events.subscribe((event) => {
-    //   if (event instanceof NavigationEnd) {
-    //     const urlSegments = event.url.split('/');
-    //     this.section = urlSegments[1];
-    //     this.sectionTitle = this.section;
-    //   }
-    // });
-
-  }
+  ngOnInit() {}
 
   ngAfterViewInit() {
 
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
+
         const urlSegments = event.url.split('/');
         this.section = urlSegments[1];
         this.sectionTitle = this.section;
-      }
-    })
 
-    this.pagesService.getAll().subscribe((res) => {
-        res['items'].filter((item) => {
-          if (item.meta.slug === this.section) {
-            if (item.meta.children.children.length === 1 && item.meta.children.children[0].children_count == 0) {
-              this.grandChildren = false
-            }
-            this.sidebarNav = item.meta.children.children.sort((thisChild, nextChild) => {
-              item.meta.children.children.map(child => {
-                child.children.length <= 0 ? this.grandChildren = false : this.grandChildren = true;
-                child.children.sort((thisGrandChild, nextGrandchild) => {
-                  return thisGrandChild.title > nextGrandchild.title ? 1 : -1;
+        this.loading = true;
+        this.pagesService.getAll().subscribe(
+          (res) => {
+            res['items'].filter((item) => {
+              if (item.meta.slug === this.section) {
+                if (item.meta.children.children.length === 1 && item.meta.children.children[0].children_count == 0) {
+                  this.grandChildren = false
+                }
+                this.sidebarNav = item.meta.children.children.sort((thisChild, nextChild) => {
+                  item.meta.children.children.map(child => {
+                    child.children.length <= 0 ? this.grandChildren = false : this.grandChildren = true;
+                    child.children.sort((thisGrandChild, nextGrandchild) => {
+                      return thisGrandChild.title > nextGrandchild.title ? 1 : -1;
+                    });
+                  });
+                  if (this.grandChildren) {
+                    return thisChild.menu_order > nextChild.menu_order ? 1 : -1;
+                  } else {
+                    return thisChild.title > nextChild.title ? 1 : -1;
+                  }
                 });
-              });
-              if (this.grandChildren) {
-                return thisChild.menu_order > nextChild.menu_order ? 1 : -1;
-              } else {
-                return thisChild.title > nextChild.title ? 1 : -1;
               }
             });
+          },
+          () => {
+            this.loading = false;
+          },
+          () => {
+            this.loading = false;
           }
-        });
-      })
+        )
+      }
+    })
 
   }
 
