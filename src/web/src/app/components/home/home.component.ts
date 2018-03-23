b71301f69b74939814ce6c0b922117fef63bb347
John Macaluso <John.Macaluso@infor.com>
2018-03-23 14:22:26 -0400

    dot animation

--- src/web/src/app/components/home/home.component.ts
+++ src/web/src/app/components/home/home.component.ts
@@ -10,6 +10,7 @@ import { PagesService } from '../../shared/pages.service';
 export class HomeComponent implements AfterViewInit {
   @Input() page;
   @ViewChild('whiteDotPattern') whiteDotPattern:ElementRef;
+  public dotPatternPaths;
   public pageContent: any;
   public loading = true;
 
@@ -29,15 +30,8 @@ export class HomeComponent implements AfterViewInit {
 
     });
 
-    const dotPatternPaths = this.whiteDotPattern.nativeElement.children[0].children;
-
-    for (let i = 0; i < dotPatternPaths.length; i++) {
-      dotPatternPaths[i].style.transform = `translate(-${Math.floor(Math.random() * 100)}px, ${Math.floor(Math.random() * 100)}px)`;
-    }
-
-    setTimeout(() => {
-      this.whiteDotPattern.nativeElement.classList.add('white-dot-pattern--loaded');
-    }, 100);
+    this.dotPatternPaths = this.whiteDotPattern.nativeElement.children[0].children;
+    this.animateDots(this.dotPatternPaths);
 
   }
 
@@ -73,6 +67,37 @@ export class HomeComponent implements AfterViewInit {
         }
     );
 
+  }
+
+  private animateDots(pattern) {
+
+    for (let i = 0; i < pattern.length; i++) {
+      pattern[i].classList.add(`dot--${i}`);
+      pattern[i].style.transform = `translate(-${Math.floor(Math.random() * 100)}px, ${Math.floor(Math.random() * 100)}px)`;
+    }
+
+    setTimeout(() => {
+      for (let i = 0; i < pattern.length; i++) {
+        pattern[i].style.transform = `translate(0px, 0px)`;
+      }
+    }, 100);
+
+    setTimeout(() => {
+      this.whiteDotPattern.nativeElement.classList.add('white-dot-pattern--settled');
+    }, 2300);
+
+    setInterval(() => {
+      setPulseClass()
+    }, 2000);
+
+    function setPulseClass() {
+      const numberOfDots = pattern.length;
+      let random = Math.floor((Math.random() * 279) + 1);
+      [].slice.call(pattern).forEach(el => {
+        el.classList.remove('dot--pulse');
+      });
+      pattern[random].classList.add('dot--pulse');
+    }
   }
 
 }
