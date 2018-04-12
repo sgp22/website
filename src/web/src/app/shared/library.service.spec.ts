af05aa3269b500faba690e90c735fc45a6056de8
Christopher Lepore <chris.lepore@infor.com>
2018-04-12 17:30:27 -0400

    Added library service spec

---
+++ src/web/src/app/shared/library.service.spec.ts
@@ -0,0 +1,52 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { TestBed } from '@angular/core/testing';
+
+import { CacheService } from './cache.service';
+import { LibraryService } from './library.service';
+
+const DOMAIN_DOCS_API = 'domain';
+
+fdescribe('LibraryService', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  let libraryService: LibraryService;
+
+  beforeEach(() => {
+
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
+      providers: [
+        CacheService,
+        LibraryService
+      ]
+    });
+
+    libraryService = TestBed.get(LibraryService);
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
+  });
+
+  describe('#getAllLibraries', () => {
+    const testUrl = `${process.env.DOMAIN_DOCS_API}/static/libraries.json`;
+    const testData = { title: 'Test' };
+
+    it('can GET all libraries', () => {
+      // Make an HTTP GET request
+      libraryService.getAllLibraries()
+        .subscribe(data =>
+          expect(data).toEqual(testData)
+        );
+
+      const req = httpTestingController.expectOne(testUrl);
+      console.log(req);
+      expect(req.request.method).toEqual('GET');
+      req.flush(testData);
+    });
+  });
+})
