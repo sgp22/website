a514297910ffe5e95605fdf8caeed8145360ba92
Christopher Lepore <chris.lepore@infor.com>
2018-04-12 17:29:10 -0400

    Added doc service spec

---
+++ src/web/src/app/shared/doc.service.spec.ts
@@ -0,0 +1,78 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { TestBed } from '@angular/core/testing';
+
+import { CacheService } from './cache.service';
+import { DocService } from './doc.service';
+
+
+describe('DocService', () => {
+
+  describe('#getDoc', () => {
+    let httpClient: HttpClient;
+    let httpTestingController: HttpTestingController;
+    let docService: DocService;
+
+    const testUrl = '/doc';
+    const testData = { title: 'Doc' };
+
+    beforeEach(() => {
+
+      TestBed.configureTestingModule({
+        imports: [ HttpClientTestingModule ],
+        providers: [
+          CacheService,
+          DocService
+        ]
+      });
+
+      docService = TestBed.get(DocService);
+
+      httpClient = TestBed.get(HttpClient);
+      httpTestingController = TestBed.get(HttpTestingController);
+    });
+
+    afterEach(() => {
+      // After every test, assert that there are no more pending requests.
+      httpTestingController.verify();
+    });
+
+    it('can GET the document data', () => {
+      docService
+        .getDoc(testUrl)
+        .subscribe(data =>
+          expect(data).toEqual(testData)
+        );
+
+      const req = httpTestingController.expectOne(testUrl);
+      expect(req.request.method).toEqual('GET');
+      req.flush(testData);
+    });
+
+    it('can handle 400 and return \'0\'', () => {
+      const emsg = 'deliberate 404 error';
+
+      docService.getDoc(testUrl).subscribe(
+        data => {
+          expect(data).toEqual('0');
+        }
+      );
+      const req = httpTestingController.expectOne(testUrl);
+      req.flush(emsg, { status: 400, statusText: '0' });
+    });
+
+    it('can handle non-404 errors and throw error', () => {
+      const emsg = 'deliberate 404 error';
+
+      docService.getDoc(testUrl).subscribe(
+        data => fail('should throw Observable Error'),
+        (error: HttpErrorResponse) => {
+          expect(error.message).toEqual('404 Not Found');
+        }
+      );
+
+      const req = httpTestingController.expectOne(testUrl);
+      req.flush(emsg, { status: 404, statusText: 'Not Found' });
+    });
+  });
+})
