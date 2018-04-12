c294847d50a3d3afe49205c84037377615e7c65d
Christopher Lepore <chris.lepore@infor.com>
2018-04-12 19:11:54 -0400

    Finished Library service spec

--- src/web/src/app/shared/library.service.spec.ts
+++ src/web/src/app/shared/library.service.spec.ts
@@ -2,25 +2,27 @@ import { HttpClientTestingModule, HttpTestingController } from '@angular/common/
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
+import { AppSettings } from '../app.settings';
 import { CacheService } from './cache.service';
 import { LibraryService } from './library.service';
 
-
-fdescribe('LibraryService', () => {
+describe('LibraryService', () => {
   let httpClient: HttpClient;
   let httpTestingController: HttpTestingController;
   let libraryService: LibraryService;
+  let appSettingsSpy: AppSettings;
 
   beforeEach(() => {
-
     TestBed.configureTestingModule({
       imports: [ HttpClientTestingModule ],
       providers: [
+        { provide: AppSettings, useValue: { domainDocsApi: 'domainDocsApi' }},
         CacheService,
         LibraryService
       ]
     });
 
+    appSettingsSpy = TestBed.get(AppSettings);
     libraryService = TestBed.get(LibraryService);
     httpClient = TestBed.get(HttpClient);
     httpTestingController = TestBed.get(HttpTestingController);
@@ -31,21 +33,33 @@ fdescribe('LibraryService', () => {
     httpTestingController.verify();
   });
 
-  describe('#getAllLibraries', () => {
-    const testUrl = `${process.env.DOMAIN_DOCS_API}/static/libraries.json`;
+  it('#getAllLibraries', () => {
+    const testUrl = `${appSettingsSpy.domainDocsApi}/static/libraries.json`;
     const testData = { title: 'Test' };
 
-    it('can GET all libraries', () => {
-      // Make an HTTP GET request
-      libraryService.getAllLibraries()
-        .subscribe(data =>
-          expect(data).toEqual(testData)
-        );
+    libraryService.getAllLibraries()
+      .subscribe(data =>
+        expect(data).toEqual(testData)
+      );
 
-      const req = httpTestingController.expectOne(testUrl);
-      console.log(req);
-      expect(req.request.method).toEqual('GET');
-      req.flush(testData);
-    });
+    const req = httpTestingController.expectOne(testUrl);
+    expect(req.request.method).toEqual('GET');
+    req.flush(testData);
   });
-})
+
+
+  it('#getAllLibraryVersionPaths', () => {
+    const testLibrary = 'testlib';
+    const testUrl = `${appSettingsSpy.domainDocsApi}/api/docs/${testLibrary}/`;
+    const testData = { title: 'Test' };
+
+    libraryService.getAllLibraryVersionPaths(testLibrary)
+      .subscribe(data =>
+        expect(data).toEqual(testData)
+      );
+
+    const req = httpTestingController.expectOne(testUrl);
+    expect(req.request.method).toEqual('GET');
+    req.flush(testData);
+  });
+});
