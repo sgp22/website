323dd25dad4ecd91c621183be16184235ddf71a6
John Macaluso <John.Macaluso@infor.com>
2018-06-05 15:52:09 -0400

    added localstorage

---
+++ src/web/src/app/components/question-widget/question-widget.component.ts
@@ -0,0 +1,13 @@
+import { Component, AfterViewInit } from '@angular/core';
+
+@Component({
+  selector: 'question-widget',
+  templateUrl: './question-widget.component.html'
+})
+export class QuestionWidgetComponent implements AfterViewInit {
+
+  constructor() { }
+
+  ngAfterViewInit() { }
+
+}
