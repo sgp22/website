fd3a6eae4063823f85e4b0a2baabd9d5e6ff5639
John Macaluso <macaluso.john@gmail.com>
2018-06-06 14:55:25 -0400

    Added hover tracking

--- src/web/src/app/components/question-widget/question-widget.component.ts
+++ src/web/src/app/components/question-widget/question-widget.component.ts
@@ -5,9 +5,15 @@ import { Component, AfterViewInit } from '@angular/core';
   templateUrl: './question-widget.component.html'
 })
 export class QuestionWidgetComponent implements AfterViewInit {
+  public widgetHovered = false;
 
   constructor() { }
 
   ngAfterViewInit() { }
+
+  captureHover(event) {
+    (<any>window).ga('send', 'event', 'feedback', 'hover', 'whatisyourrole');
+    this.widgetHovered = true;
+  }
 
 }
