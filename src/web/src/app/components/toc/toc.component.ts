f314876502a6588630606eccc207cc402a8a6224
John Macaluso <John.Macaluso@infor.com>
2018-08-22 15:44:40 -0400

    Revert "building the data for TOC in the docs component"
    This reverts commit f13f68b1f3a66ea66b3789b1c03b09d72a77cc9d.

--- src/web/src/app/components/toc/toc.component.ts
+++ src/web/src/app/components/toc/toc.component.ts
@@ -11,6 +11,7 @@ interface TocItems {
   templateUrl: './toc.component.html'
 })
 export class TocComponent implements AfterViewInit {
+  // public tocItems: TocItems[] = [];
   public landingPage;
   @Input() loading;
   @Input() component;
@@ -21,7 +22,40 @@ export class TocComponent implements AfterViewInit {
     private router: Router
   ) { }
 
-  ngAfterViewInit() {}
+  async ngAfterViewInit() {
+
+    const routeUrl = await this.route.url;
+    routeUrl.subscribe(urlSegment => {
+
+      console.log(this.tocItems);
+    //   // this.tocItems = [];
+
+    //   const lastSegment = urlSegment.slice(-1)[0].path;
+    //   if (lastSegment === 'latest' || lastSegment === 'index') {
+    //     this.landingPage = true;
+    //   } else {
+    //     this.landingPage = false;
+    //   }
+
+    //   const titles = [].slice.call(document.querySelectorAll('h2'));
+    //   titles.map((item) => {
+    //     this.tocItems.push({
+    //       label: item.innerText,
+    //       id: item.id
+    //     });
+    //   });
+    //   this.pageLoadToSection();
+
+    });
+
+  }
+
+  pageLoadToSection() {
+    const tree = this.router.parseUrl(this.router.url);
+    if (tree.fragment) {
+      this.scrollToSection(tree.fragment);
+    }
+  }
 
   clickToSection() {
     this.route.fragment.subscribe(fragment => this.scrollToSection(fragment));
