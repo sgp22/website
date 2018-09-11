eb5dead6f29339be4feddadd1138ef04ce71ac41
John Macaluso <John.Macaluso@infor.com>
2018-09-11 16:57:09 -0400

    fix formatting

--- src/web/src/app/components/search-page/search-page.component.ts
+++ src/web/src/app/components/search-page/search-page.component.ts
@@ -32,8 +32,7 @@ export class SearchPageComponent implements OnInit {
 
   ngOnInit() {
     this.domain = this.appSettings.domain;
-    this.route.queryParams.subscribe(params => {
-      this.search(params.q) });
+    this.route.queryParams.subscribe(params => this.search(params.q));
   }
 
   handleQuery(searchForm) {
@@ -51,7 +50,6 @@ export class SearchPageComponent implements OnInit {
     this.searchService.getSearch(term)
       .subscribe(
         res => {
-          console.log(res);
           const { docs, pages } = res.results;
           docs ? this.docsResults = docs.hits : this.docsResults = [];
           this.pagesResults = pages;
