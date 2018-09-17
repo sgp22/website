e4a49642c287103d8405d5f0f52e77961f1294ef
corpulent <artemgolub@gmail.com>
2018-09-17 13:34:02 -0400

    Enable optional debug for staging and dev.

--- src/app/app/settings/dev.py
+++ src/app/app/settings/dev.py
@@ -3,7 +3,9 @@ from __future__ import absolute_import, unicode_literals
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv(
+    'DEBUG',
+    'true').lower() == 'true'
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'z4w6&+0zrxxx#6-8m9w8!s*ms1f#@0y%18+x!ct=w&s7lyn%v&'
