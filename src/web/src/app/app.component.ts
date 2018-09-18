857da5a4a02281e0dd928d73119897d583abca57
John Macaluso <John.Macaluso@infor.com>
2018-09-18 17:20:32 -0400

    fix history bug due to new search route

--- src/web/src/app/app.component.ts
+++ src/web/src/app/app.component.ts
@@ -29,8 +29,13 @@ export class AppComponent {
 
     router.events.subscribe( (event) => {
       if (event instanceof NavigationEnd) {
-
         const url = router.routerState.snapshot.url;
+
+        const urlSegments = event.url.split('/');
+        (urlSegments[1] === 'code') ? this.codeSection = true : this.codeSection = false;
+        (url === '/' || url === '/search' || url.includes('/search?q=')) ? this.useGrid = false : this.useGrid = true;
+        (urlSegments[1] === 'blog') ? this.blog = true : this.blog = false;
+
         const title = url.replace(/^\//g, '').replace(/\//g, ' / ').replace(/-/g, ' ');
         if (url === '/') {
           this.home = true;
@@ -44,11 +49,6 @@ export class AppComponent {
           }
           titleService.setTitle(`${this.capitalizeTitle(title)} - Infor Design System`);
         }
-
-        const urlSegments = event.url.split('/');
-        (urlSegments[1] === 'code') ? this.codeSection = true : this.codeSection = false;
-        (url === '/' || url === '/search' || url.includes('/search?')) ? this.useGrid = false : this.useGrid = true;
-        (urlSegments[1] === 'blog') ? this.blog = true : this.blog = false;
 
 
         // Initialize Pendo on page change
