7b5d026b08f39f788bd64b67fe24676bd3892056
John Macaluso <John.Macaluso@infor.com>
2018-09-28 10:32:06 -0400

    added update view

--- src/app/feedback/urls.py
+++ src/app/feedback/urls.py
@@ -4,4 +4,5 @@ from feedback import views
 
 urlpatterns = [
     url(r'^thumbs/$', views.FeedbackThumbs.as_view()),
+    url(r'^thumbs/(?P<pk>[0-9]+)/$', views.UpdataFeedbackThumbs.as_view()),
 ]
--- src/app/feedback/views.py
+++ src/app/feedback/views.py
@@ -19,3 +19,23 @@ class FeedbackThumbs(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+class UpdataFeedbackThumbs(APIView):
+    def get_object(self, pk):
+        try:
+            return Feedback.objects.get(pk=pk)
+        except Feedback.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        feedback = self.get_object(pk)
+        serializer = FeedbackSerializer(feedback)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        feedback = self.get_object(pk)
+        serializer = FeedbackSerializer(feedback, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
