95ef472dd1d8c1d945d0e90704f90024fbe42607
John Macaluso <John.Macaluso@infor.com>
2018-09-28 10:08:33 -0400

    working post and get thumb api

--- src/app/feedback/urls.py
+++ src/app/feedback/urls.py
@@ -3,6 +3,5 @@ from django.conf.urls import url
 from feedback import views
 
 urlpatterns = [
-    url(r'^thumbs-up', views.FeedbackThumbsUp.as_view()),
-    url(r'^thumbs-down', views.FeedbackThumbsDown.as_view())
+    url(r'^thumbs/$', views.FeedbackThumbs.as_view()),
 ]
--- src/app/feedback/views.py
+++ src/app/feedback/views.py
@@ -6,7 +6,7 @@ from django.http import Http404
 from .models import Feedback
 from .serializers import FeedbackSerializer
 
-class FeedbackThumbsUp(APIView):
+class FeedbackThumbs(APIView):
     def get(self, request, **kwargs):
         feedback = Feedback.objects.all()
         serializer = FeedbackSerializer(feedback, many=True)
@@ -14,20 +14,7 @@ class FeedbackThumbsUp(APIView):
 
     def post(self, request, **kwargs):
         serializer = FeedbackSerializer(data=request.data)
-        if serializer.isValid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class FeedbackThumbsDown(APIView):
-    def get(self, request, **kwargs):
-        feedback = Feedback.objects.all()
-        serializer = FeedbackSerializer(feedback, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, **kwargs):
-        serializer = FeedbackSerializer(data=request.data)
-        if serializer.isValid():
+        if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
