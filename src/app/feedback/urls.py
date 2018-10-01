188a9b07e9457c22e1bbafe4c3ab2babb784b4e5
John Macaluso <John.Macaluso@infor.com>
2018-10-01 13:27:24 -0400

    url pattern for thumbs by page

--- src/app/feedback/urls.py
+++ src/app/feedback/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from feedback import views
 
 urlpatterns = [
+    url(r'^thumbs/.*/$', views.FeedbackThumbs.as_view()),
     url(r'^thumbs-up/$', views.FeedbackThumbs.as_view()),
     url(r'^thumbs-down/$', views.FeedbackThumbs.as_view()),
 ]
