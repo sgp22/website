5039b6ecdab5fd5f6089790c6bbc4807fc6281f9
John Macaluso <John.Macaluso@infor.com>
2018-10-01 11:57:00 -0400

    revert to exact urlpattern

--- src/app/feedback/urls.py
+++ src/app/feedback/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from feedback import views
 
 urlpatterns = [
-    url(r'^thumbs/.*/$', views.FeedbackThumbs.as_view()),
-    url(r'^thumbs/.*/(?P<pk>[0-9]+)/$', views.UpdataFeedbackThumbs.as_view()),
+    url(r'^thumbs-up/$', views.FeedbackThumbs.as_view()),
+    url(r'^thumbs-down/$', views.FeedbackThumbs.as_view()),
 ]
