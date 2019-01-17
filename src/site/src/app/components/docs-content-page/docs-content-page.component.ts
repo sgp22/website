4600ce677af5fb4f9b4e7c843bc8866018d360c8
John Macaluso <John.Macaluso@infor.com>
2019-01-17 16:32:48 -0500

    relative links

--- src/site/src/app/components/docs-content-page/docs-content-page.component.ts
+++ src/site/src/app/components/docs-content-page/docs-content-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute }from '@angular/router';
+import { ActivatedRoute, Router }from '@angular/router';
 import { DocsService } from './docs.service';
 import { LibraryService } from '../../shared/library.service';
 import { DomSanitizer } from '@angular/platform-browser';
@@ -24,6 +24,7 @@ export class DocsContentPageComponent implements OnInit {
     private docsService: DocsService,
     private libraryService: LibraryService,
     private route: ActivatedRoute,
+    private router: Router,
     private sanitizer: DomSanitizer
   ) { }
 
@@ -73,6 +74,7 @@ export class DocsContentPageComponent implements OnInit {
         .subscribe(res => {
 
           this.docs = res;
+          this.handleRelativeLinks(this.docs);
 
           if (res['demo']) {
             if (res['demo'].pages) {
@@ -85,7 +87,6 @@ export class DocsContentPageComponent implements OnInit {
             if (res['demo'].embedded) {
               res['demo'].embedded.forEach(page => {
                 page.githubUrl = this.createGithubUrl(page.slug);
-                page.url = this.createDemoUrl(page.slug);
                 page.trustedUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.createDemoUrl(page.slug, true));
               });
             }
@@ -95,6 +96,77 @@ export class DocsContentPageComponent implements OnInit {
         });
 
     })
+  }
+
+  handleRelativeLinks(docs) {
+    // Relative Link support.
+    // tempNode needs to be created in order to convert
+    // the fragment back to a string
+    const tempNode = document.createElement('div');
+    const bodyFragments = document.createRange().createContextualFragment(docs.body);
+    const allHrefs = Array.from(bodyFragments.querySelectorAll('a'));
+    const allImgs = Array.from(bodyFragments.querySelectorAll('img'));
+    const allIframes = Array.from(bodyFragments.querySelectorAll('iframe'));
+
+    // Modify relative hrefs, img src and iframe src.
+    if (allHrefs.length) {
+      allHrefs.map(a => this.createRelativePath(a, 'href'));
+    }
+
+    if (allImgs.length) {
+      allImgs.map(img => this.createRelativePath(img, 'src'));
+    }
+
+    if (allIframes.length) {
+      allIframes.map(iframe => this.createRelativePath(iframe, 'src'));
+    }
+
+    // Append the modified fragment to the tempNode
+    // and assign the innerHTML to the template var
+    tempNode.appendChild(bodyFragments);
+    docs.bodyTrustedHtml = this.sanitizer.bypassSecurityTrustHtml(tempNode.innerHTML);
+  }
+
+  createRelativePath(el, attr, navigate = false) {
+    const absolute = /^((http|https|ftp):\/\/)/;
+    if (el.getAttribute(attr)) {
+      if (!absolute.test(el.getAttribute(attr))) {
+        if (navigate) {
+          const relativeLink = el.getAttribute(attr);
+          const pathArray = relativeLink.split('/');
+          if (pathArray[1] === 'code' && pathArray[4] === 'demo') {
+            // pass, behave like normal following a click behavior
+          } else {
+            event.preventDefault();
+            this.router.navigate([`${relativeLink}`]);
+          }
+        } else {
+          const relativeHref = el.getAttribute(attr).replace(/(^\.\/|.html$)/g, '');
+          const pathArray = relativeHref.split('/');
+          if (pathArray.includes('demo')) {
+            el.setAttribute('target', '_blank');
+          }
+          if (relativeHref.substring(0, 1) === '/') {
+            // Relative to the root of the domain
+            el.setAttribute(attr, `${relativeHref}`);
+          } else {
+            // Relative to the current path
+            el.setAttribute(attr, `${this.absolutePath}/${relativeHref}`);
+          }
+        }
+      }
+    }
+  }
+
+  relativeLinks(link) {
+    const absolute = /^((http|https|ftp):\/\/)/;
+    const el = event.target as HTMLElement;
+    const href = el.getAttribute('href');
+    if (!absolute.test(href)) {
+      if (el.tagName.toLowerCase() === 'a') {
+        this.createRelativePath(el, 'href', true);
+      }
+    }
   }
 
   createDemoUrl(slug: string, embeddedLayout: boolean = false) {
