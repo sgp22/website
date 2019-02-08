c9d15a072d304e1183748d3e072f391cf044bf46
John Macaluso <John.Macaluso@infor.com>
2019-02-08 13:01:19 -0500

    Added tests

--- src/site/src/app/components/docs-content-page/docs.service.spec.ts
+++ src/site/src/app/components/docs-content-page/docs.service.spec.ts
@@ -1,12 +1,50 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
-
+import { environment } from '../../../environments/environment';
+import { CacheService } from '../../shared/cache.service';
 import { DocsService } from './docs.service';
 
-describe('DocsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: DocsService = TestBed.get(DocsService);
-    expect(service).toBeTruthy();
+describe('DocsService', () => {
+
+  describe('#loadDocs', () => {
+    let httpClient: HttpClient;
+    let httpTestingController: HttpTestingController;
+    let docsService: DocsService;
+
+    beforeEach(() => {
+
+      TestBed.configureTestingModule({
+        imports: [HttpClientTestingModule],
+        providers: [
+          CacheService,
+          DocsService
+        ]
+      });
+
+      docsService = TestBed.get(DocsService);
+      httpClient = TestBed.get(HttpClient);
+      httpTestingController = TestBed.get(HttpTestingController);
+    });
+
+    afterEach(() => {
+      // After every test, assert that there are no more pending requests.
+      httpTestingController.verify();
+    });
+
+    it('should GET the document data', () => {
+      const testParams = `ids-enterprise/latest/docs/index.json`;
+      const testData = { title: 'Test' };
+      docsService
+        .loadDocs(testParams)
+        .subscribe(data =>
+          expect(data).toEqual(testData)
+        );
+
+      const req = httpTestingController.expectOne(`${environment.apiUrl}/api/docs/${testParams}`);
+      expect(req.request.method).toEqual('GET');
+      req.flush(testData);
+    });
   });
 });
--- src/site/src/app/shared/library.service.spec.ts
+++ src/site/src/app/shared/library.service.spec.ts
@@ -1,12 +1,62 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
+import { environment } from '../../environments/environment';
+import { CacheService } from './cache.service';
 import { LibraryService } from './library.service';
 
 describe('LibraryService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  let libraryService: LibraryService;
 
-  it('should be created', () => {
-    const service: LibraryService = TestBed.get(LibraryService);
-    expect(service).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        CacheService,
+        LibraryService
+      ]
+    });
+
+    libraryService = TestBed.get(LibraryService);
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
+  });
+
+  it('#getAllLibraries', () => {
+    const testUrl = `${environment.apiUrl}/static/libraries.json`;
+    const testData = { title: 'Test' };
+
+    libraryService.loadAllLibraries()
+      .subscribe(data =>
+        expect(data).toEqual(testData)
+      );
+
+    const req = httpTestingController.expectOne(testUrl);
+    expect(req.request.method).toEqual('GET');
+    req.flush(testData);
+  });
+
+
+  it('#getAllLibraryVersionPaths', () => {
+    const testLibrary = 'testlib';
+    const testUrl = `${environment.apiUrl}/api/docs/${testLibrary}/`;
+    const testData = { title: 'Test' };
+
+    libraryService.loadAllLibraryVersions(testLibrary)
+      .subscribe(data =>
+        expect(data).toEqual(testData)
+      );
+
+    const req = httpTestingController.expectOne(testUrl);
+    expect(req.request.method).toEqual('GET');
+    req.flush(testData);
   });
 });
