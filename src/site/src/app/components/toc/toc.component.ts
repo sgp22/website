1551e1c979a36491550171c91e48ac2aa9bd7bd8
John Macaluso <John.Macaluso@infor.com>
2019-02-15 15:05:45 -0500

    toc slide in

--- src/site/src/app/components/toc/toc.component.ts
+++ src/site/src/app/components/toc/toc.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 interface TocItems {
   label: string;
@@ -8,7 +9,18 @@ interface TocItems {
 @Component({
   selector: 'toc-items',
   templateUrl: './toc.component.html',
-  styleUrls: ['./toc.component.scss']
+  styleUrls: ['./toc.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+      transition(':enter', [
+        style({ transform: 'translateX(100%)' }),
+        animate('200ms ease', style({ transform: 'translateX(0%)' }))
+      ]),
+      transition(':leave', [
+        animate('200ms ease', style({ transform: 'translateX(100%)' }))
+      ])
+    ])
+  ]
 })
 export class TocComponent implements OnInit {
   public landingPage;
