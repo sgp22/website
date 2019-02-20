65f44a64cf8437b3f7259573f5f52dd24a3b6ead
John Macaluso <John.Macaluso@infor.com>
2019-02-20 11:21:52 -0500

    animations file

---
+++ src/site/src/app/animations.ts
@@ -0,0 +1,22 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
+
+export const slideInOut = trigger('slideInOut', [
+  transition(':enter', [
+    style({ transform: 'translateX(100%)' }),
+    animate('200ms ease', style({ transform: 'translateX(0%)' }))
+  ]),
+  transition(':leave', [
+    animate('200ms ease', style({ transform: 'translateX(100%)' }))
+  ])
+])
+
+export const fadeInOut = trigger('thankyouState', [
+  state('false', style({
+    opacity: 1
+  })),
+  state('true', style({
+    opacity: 0
+  })),
+  transition('false => true', animate('200ms ease-out')),
+  transition('true => false', animate('200ms ease-in'))
+]);
