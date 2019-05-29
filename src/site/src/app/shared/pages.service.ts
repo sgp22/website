32ee295f0f6d296c46a6cc5361cac41b59f11f38
Nick Wynja <nick@nickwynja.com>
2019-05-29 12:07:14 -0400

    Update to hookandloop medium group

--- src/site/src/app/shared/pages.service.ts
+++ src/site/src/app/shared/pages.service.ts
@@ -89,7 +89,7 @@ export class PagesService {
   }
 
   getMediumFeed() {
-    const url = `https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40hookandloop`;
+    const url = `https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2Fhookandloop`;
     return this.cacheService.get(url, this.http.get(url));
   }
 }
