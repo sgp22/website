2e180c0124612c22dccabc9b12763628c9045efc
Nick Wynja <nick@nickwynja.com>
2019-05-29 12:22:09 -0400

    Use innerHTML to render html entities

--- src/site/src/app/components/blog-related-posts/medium-posts.component.ts
+++ src/site/src/app/components/blog-related-posts/medium-posts.component.ts
@@ -14,9 +14,9 @@ import { Component, OnInit, Input } from '@angular/core';
           <div class="blog-post--content">
             <span class="blog-post--meta">
               <time class="blog-post--date">{{post.pubDate | date: 'MMMM d, yyyy'}}</time> |
-              <span class="blog-post--author">{{post.author}}</span>
+              <span class="blog-post--author" [innerHTML]="post.author"></span>
             </span>
-            <h1 class="blog-post--title">{{post.title}}</h1>
+            <h1 class="blog-post--title" [innerHTML]="post.title"></h1>
             <span class="blog-post--readmore">Read More <svg class="ids-icon"><use xlink:href="#icon-drilldown"/></svg></span>
           </div>
         </a>
