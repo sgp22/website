2554d7b531f2179580f8c320f0222cc8e059b2bf
John Macaluso <John.Macaluso@infor.com>
2019-06-19 14:34:03 -0400

    Remove old preview hooks from backend

--- src/app/app/static/js/editor.js
+++ src/app/app/static/js/editor.js
@@ -1,34 +1,3 @@
 $(function () {
 
-  // const host = window.location.hostname;
-  // const urlPath = $("header .row .status-tag:contains('live')").attr('href');
-  // const num = /\d{1,10}/g;
-  // const id = window.location.pathname.match(num).toString();
-  // const button = $('<button>');
-  // const unpublishBtn = $("footer ul a:contains('Unpublish')");
-
-  // /*
-  //   Remove default preview button
-  // */
-  // $('.action-preview').remove();
-
-  // /*
-  //   Create our button
-  // */
-  // button
-  //   .addClass('button icon icon-view')
-  //   .text('Preview')
-  //   .attr('data-action', `http://${host}${urlPath}?preview=true&id=${id}`);
-
-  // if (unpublishBtn.length) {
-
-  //   $('.preview').append(button);
-
-  //   button.on('click', function(e) {
-  //     e.preventDefault();
-  //     window.open( $(this).data('action') );
-  //   });
-
-  // }
-
 });
